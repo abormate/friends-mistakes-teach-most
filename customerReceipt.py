720139cd80e0b395c08d101672cccd6d5ebc031f
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2021-06-22 17:15:40 +0800

    Python Code
    Practicing skills in writing logic with Python with simple, informative programs

---
+++ customerReceipt.py
@@ -0,0 +1,52 @@
+# description for the chair
+lovely_loveseat_description = """
+Lovely Loveseat. Tufted polyester blend on wood. 32 inches high x 40 inches wide x 30 inches deep. Red or white.
+"""
+
+# price of the seat
+lovely_loveseat_price = 254.00
+
+# another chair
+stylish_settee_description = """
+Stylish Settee. Faux leather on birch. 29.50 inches high x 54.75 inches wide x 28 inches deep. Black.
+"""
+
+# stylish settee price
+stylish_settee_price = 180.50
+
+# luxurious lamp
+luxurious_lamp_description = """
+Luxurious Lamp. Glass and iron. 36 inches tall. Brown with cream shade.
+"""
+
+luxurious_lamp_price = 52.15
+
+# sales tax
+sales_tax = 0.088
+
+# customer 1 tally of expenses
+customer_one_total = 0
+
+# list of descriptions of things theyre purchasing
+customer_one_itemization = ""
+
+# customer one gets the lovely loveseat
+customer_one_total += lovely_loveseat_price
+
+# add description of items purchased
+customer_one_itemization += lovely_loveseat_description
+
+# customer one adds lamp to purchase list
+customer_one_total += luxurious_lamp_price
+
+# add lamp description to itemization for customer one
+customer_one_itemization += luxurious_lamp_description
+
+# sales tax for customer one
+customer_one_tax = customer_one_total * sales_tax
+
+# total price = sales tax + cost of items
+customer_one_total += customer_one_tax
+
+# start printing the customer receipt
+print("Customer One Items", customer_one_itemization, "Customer One Total", customer_one_total)
---
+++ planetWeightGravity.py
@@ -0,0 +1,27 @@
+print("I have information for the following planets:\n")
+
+print("   1. Venus   2. Mars    3. Jupiter")
+print("   4. Saturn  5. Uranus  6. Neptune\n")
+
+weight = 185
+planet = 3
+
+# Write an if statement below:
+if planet == 1:
+  weight *= 0.91
+  print("Codey's weight is " + str(weight) + " on Venus")
+elif planet == 2:
+  weight *= 0.38
+  print("Codey's weight is " + str(weight) + " on Mars")
+elif planet == 3:
+  weight *= 2.34
+  print("Codey's weight is " + str(weight) + " on Jupiter")
+elif planet == 4:
+  weight *= 1.06
+  print("Codey's weight is " + str(weight) + " on Saturn")
+elif planet == 5:
+  weight *= 0.92
+  print("Codey's weight is " + str(weight) + " on Uranus")
+else:
+  weight *= 1.19
+  print("Codey's weight is " + str(weight) + " on Neptune")
