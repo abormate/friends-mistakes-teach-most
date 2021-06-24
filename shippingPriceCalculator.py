1a838b17d2a9f17768691bde80edca9d016e9557
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2021-06-25 04:56:16 +0800

    Python Code
    A basic calculator that lets you compare different price of shipping methods.

---
+++ shippingPriceCalculator.py
@@ -0,0 +1,34 @@
+weight = 41.5
+
+# Ground shipping
+
+if weight <= 2:
+  cost = (weight * 1.50) + 20
+elif weight <= 6:
+  cost = (weight * 3.00) + 20
+elif weight <= 10:
+  cost = (weight * 4.00) + 20
+elif weight > 10:
+  cost = (weight * 4.75) + 20
+else:
+  print('Error!')
+
+print("Regular shipping: " + str(cost) + "\n")
+# Premium Shipping
+
+cost_premium = 125.0
+print("Premium shipping costs US$ 125 \n")
+
+# Drone shipping
+if weight <= 2:
+  cost = (weight * 4.50)
+elif weight <= 6:
+  cost = (weight * 9.00)
+elif weight <= 10:
+  cost = (weight * 12.00)
+elif weight > 10:
+  cost = (weight * 14.25)
+else:
+  print('Error!')
+
+print('Drone shipping costs: ' + str(cost))
