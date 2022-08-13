0d9aa593a3dbf6c27b03eb93de37e31adf6a87cb
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-08-13 18:11:14 +0800

    Update numberRaised.py

--- Python/numberRaised.py
+++ Python/numberRaised.py
@@ -1,4 +1,4 @@
-// Outputs true if base number raised with another number equals to greater than 5K
+# Outputs true if base number raised with another number equals to greater than 5K
 
 def large_power(base, exponent):
   if base ** exponent > 5000:
