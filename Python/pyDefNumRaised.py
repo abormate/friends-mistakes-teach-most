bcac22320807703220ea60fd3d0169ab9f6e8371
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-08-14 12:37:08 +0800

    Update and rename numberRaised.py to pyDefNumRaised.py

--- Python/pyDefNumRaised.py
+++ Python/pyDefNumRaised.py
@@ -1,4 +1,4 @@
-# Outputs true if base number raised with another number equals to greater than 5K
+# Outputs true if base number raised with another number equals to greater than 5,000
 
 def large_power(base, exponent):
   if base ** exponent > 5000:
