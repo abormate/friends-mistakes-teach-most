d48257cf1d6eb96af818bbe15bbc0ecd8dbbfe4a
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-08-15 10:19:24 +0800

    Update and rename pyDefBudgetOverOrNot.py to pyDefOverBudgetOrNot.py

--- Python/pyDefOverBudgetOrNot.py
+++ Python/pyDefOverBudgetOrNot.py
@@ -1,4 +1,4 @@
-# Outputs if one's expenses has already gone over budget
+# This Python function outputs if one's expenses has already gone over budget
 
 def over_budget(budget, food_bill, electricity_bill, internet_bill, rent):
   expenses = food_bill + electricity_bill + internet_bill + rent
