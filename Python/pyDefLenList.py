25695c1c23514a95bbc703d4cb8c79d8d934d91f
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-08-16 06:46:46 +0800

    Update pyDefLenList.py

--- Python/pyDefLenList.py
+++ Python/pyDefLenList.py
@@ -1,3 +1,5 @@
+# Python function that evaluates lists item/element quantities and returns the list that has more elements inside.
+
 def larger_list(lst1, lst2):
   if len(lst1) >= len(lst2):
     return lst1[-1]
