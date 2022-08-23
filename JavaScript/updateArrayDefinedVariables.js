dfec2a1e96b5080a02c5b031bf373db98a049f24
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-08-23 14:40:56 +0800

    Update updateArrayDefinedVariables.js

--- JavaScript/updateArrayDefinedVariables.js
+++ JavaScript/updateArrayDefinedVariables.js
@@ -1,3 +1,5 @@
+// JavaScript arrays, depending on how they are defined, whether with a "let" or "const" variable classes -- also will vary with how much you can update or change their values. 
+
 let condiments = ['Ketchup', 'Mustard', 'Soy Sauce', 'Sriracha'];
 
 const utensils = ['Fork', 'Knife', 'Chopsticks', 'Spork'];
