9bc07b47c028103389e02e76bf8104805988ffd7
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-08-24 15:54:29 +0800

    Update arrayMethod.js

--- JavaScript/arrayMethod.js
+++ JavaScript/arrayMethod.js
@@ -3,3 +3,5 @@ const itemTracker = ['item 0', 'item 1', 'item 2'];
 itemTracker.push('item 3', 'item 4');
  
 console.log(itemTracker); 
+
+// Output: ['item 0', 'item 1', 'item 2', 'item 3', 'item 4'];
