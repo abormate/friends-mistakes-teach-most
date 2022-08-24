1b26df4cf3bc22bbad991591c9f31082752701d9
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-08-24 17:04:24 +0800

    Update arrayMethodShift.js

--- JavaScript/arrayMethodShift.js
+++ JavaScript/arrayMethodShift.js
@@ -1,3 +1,5 @@
+// The shift() method removes the first element from an array and returns that removed element. This method changes the length of the array.
+
 const groceryList = ['orange juice', 'bananas', 'coffee beans', 'brown rice', 'pasta', 'coconut oil', 'plantains'];
 groceryList.shift();
 console.log(groceryList);
