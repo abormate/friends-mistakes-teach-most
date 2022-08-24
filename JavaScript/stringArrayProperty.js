c2ac63d005eb6d6eab345349b06466f67bee9471
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-08-24 15:51:31 +0800

    Update stringArrayProperty.js

--- JavaScript/stringArrayProperty.js
+++ JavaScript/stringArrayProperty.js
@@ -1,2 +1,4 @@
+// You can determine the number of elements from inside an Array through invoking the string property ".length"
+
 onst objectives = ['Learn a new language', 'Read 52 books', 'Run a marathon'];
 console.log(objectives.length);
