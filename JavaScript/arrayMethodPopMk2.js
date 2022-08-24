87225738e5dae66a116a3829fef39b3da6e84099
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-08-24 16:40:17 +0800

    Update arrayMethodPopMk2.js

--- JavaScript/arrayMethodPopMk2.js
+++ JavaScript/arrayMethodPopMk2.js
@@ -1,3 +1,5 @@
-onst chores = ['wash dishes', 'do laundry', 'take out trash', 'cook dinner', 'mop floor'];
+// JavaScript method for Array that doesn't take arguments, but only removes the last element from an Array
+
+const chores = ['wash dishes', 'do laundry', 'take out trash', 'cook dinner', 'mop floor'];
 chores.pop();
 console.log(chores);
