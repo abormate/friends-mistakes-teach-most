d07870c88aaeda9a737d1760da72281f8bc5b284
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-08-24 17:07:37 +0800

    Create arrayMethodUnshift.js

---
+++ JavaScript/arrayMethodUnshift.js
@@ -0,0 +1,6 @@
+// The unshift() method adds one or more elements to the beginning of an array and returns the new length of the array.
+
+const groceryList = ['orange juice', 'bananas', 'coffee beans', 'brown rice', 'pasta', 'coconut oil', 'plantains'];
+
+groceryList.unshift('cheeseburgers');
+console.log(groceryList);
