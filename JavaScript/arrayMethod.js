a6f669e0ad5b503f08b53a5f3496662134472700
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-08-24 15:55:07 +0800

    Update arrayMethod.js

--- JavaScript/arrayMethod.js
+++ JavaScript/arrayMethod.js
@@ -1,3 +1,7 @@
+/*
+built-in JavaScript methods that make working with arrays easier. These methods are specifically called on arrays to make common tasks, like adding and removing elements, more straightforward.
+*/
+
 const itemTracker = ['item 0', 'item 1', 'item 2'];
  
 itemTracker.push('item 3', 'item 4');
