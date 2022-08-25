3384465ce8f46a5246ad52362d89b6e3e0781e25
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-08-25 13:40:28 +0800

    Update selectNestedArrayElement.js

--- JavaScript/selectNestedArrayElement.js
+++ JavaScript/selectNestedArrayElement.js
@@ -1,3 +1,5 @@
+// JavaScript allows you to define arrays within an array. And you can select individual values or elements from nested arrays via chaining index selectors.
+
 const numberClusters = [[1,2], [3,4], [5,6], [7,8]];
 const target = numberClusters[3][0];
 console.log(target);
