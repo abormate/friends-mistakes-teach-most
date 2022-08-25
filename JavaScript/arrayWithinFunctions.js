a3776526e5fc73078110336ab0ce79d2aa325c06
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-08-25 10:12:38 +0800

    Update arrayWithinFunctions.js

--- JavaScript/arrayWithinFunctions.js
+++ JavaScript/arrayWithinFunctions.js
@@ -1,3 +1,8 @@
+/*
+What happens if we try to change an array inside a function? Does the array keep the change after the function call or is it scoped to inside the function?
+
+*/
+
 const flowers = ['peony', 'daffodil', 'marigold'];
  
 function addFlower(arr) {
