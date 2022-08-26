10005c18acb0278cbf2214aa8dc19ef5eaf973d1
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-08-26 10:09:27 +0800

    Update arrayMessageDifferentMethods.js

--- JavaScript/arrayMessageDifferentMethods.js
+++ JavaScript/arrayMessageDifferentMethods.js
@@ -1,3 +1,5 @@
+// JavaScript exercise using Array methods to consolidate and make sense of the array message variable defined earlier
+
 let message = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
 console.log(message.length);
 message.pop();
