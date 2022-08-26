9f0c00d86d4edb7c3ea3be70de9b05dc03e59ef5
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-08-26 10:08:33 +0800

    Create arrayMessageDifferentMethods.js

---
+++ JavaScript/arrayMessageDifferentMethods.js
@@ -0,0 +1,14 @@
+let message = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+console.log(message.length);
+message.pop();
+console.log(message.length);
+message.push('to', 'Program');
+console.log(message.length);
+message[7] = 'right';
+console.log(message[7]);
+message.shift();
+console.log(message.length);
+message.unshift('Programming');
+
+message.splice(6, 5, 'know');
+console.log(message.join());
