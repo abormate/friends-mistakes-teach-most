239f00ae1ed4cd7a0b55487f0f846af4bb392db2
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-08-30 16:36:13 +0800

    Update vacationLoopSpots.js

--- JavaScript/vacationLoopSpots.js
+++ JavaScript/vacationLoopSpots.js
@@ -3,3 +3,5 @@ vacationSpots = ['Switzerland', 'Japan', 'Singapore'];
 console.log(vacationSpots[0]);
 console.log(vacationSpots[1]);
 console.log(vacationSpots[2]);
+
+// Develop an appreciation for programming loops or iterative steps
