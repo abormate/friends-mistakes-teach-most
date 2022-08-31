ef62dabac91bcfc433753bdace3abc72d71d82b2
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-08-31 12:42:04 +0800

    Update forLoopArrayIteratorLen.js

--- JavaScript/forLoopArrayIteratorLen.js
+++ JavaScript/forLoopArrayIteratorLen.js
@@ -2,3 +2,5 @@ const vacationSpots = ['Israel', 'Japan', 'New Zealand'];
 for (let i = 0; i < vacationSpots.length; i++) {
   console.log('I would love to visit ' + vacationSpots[i]);
 }
+
+// Outputs each item of the list or array define early on. With the array length as the stopping condition for the iterator variable.
