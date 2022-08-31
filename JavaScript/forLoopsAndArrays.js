76cdb3d9bca8f7f34205707183b13c7b9e90e757
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-08-31 09:43:31 +0800

    Update forLoopsAndArrays.js

--- JavaScript/forLoopsAndArrays.js
+++ JavaScript/forLoopsAndArrays.js
@@ -2,3 +2,5 @@ const animals = ['Grizzly Bear', 'Sheep', 'Sea Lion'];
 for (let i = 0; i < animals.length; i++){
   console.log(animals[i]);
 }
+
+// Outputs the list of animals defined inside the array
