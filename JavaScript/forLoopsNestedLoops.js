142ea4828da1d411f8e3b400e26b76120560fdd8
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-08-31 13:01:28 +0800

    Update forLoopsNestedLoops.js

--- JavaScript/forLoopsNestedLoops.js
+++ JavaScript/forLoopsNestedLoops.js
@@ -1,3 +1,5 @@
+// One way nested for loops are utilized is to compare values of one array with another.
+
 const myArray = [7, 19, 20];
 const yourArray = [19, 81, 2];
 for (let i = 0; i < myArray.length; i++) {
