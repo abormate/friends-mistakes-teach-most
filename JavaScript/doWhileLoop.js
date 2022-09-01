15d45630e4c2a3346a27a88028c2da980e4f9e59
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-09-01 15:52:14 +0800

    Update doWhileLoop.js

--- JavaScript/doWhileLoop.js
+++ JavaScript/doWhileLoop.js
@@ -1,3 +1,4 @@
+// Do While loops run their code block at least once even if the stopping iterator variable evaluates to false at the fist iteration. Unlike regular While loops 
 
 let cupsOfSugarNeeded = 0;
 let cupsAdded = 0;
