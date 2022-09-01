4f73bec2a5203a37aa025a392418bbdd0fe7af56
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-09-01 15:31:31 +0800

    Update whileLoopCardNotSpade.js

--- JavaScript/whileLoopCardNotSpade.js
+++ JavaScript/whileLoopCardNotSpade.js
@@ -6,3 +6,5 @@ while (currentCard != 'spade') {
   currentCard = cards[Math.floor(Math.random() *4)];
   console.log(currentCard);
 };
+
+// Checks and changes card at hand until card suit of Spades is reached
