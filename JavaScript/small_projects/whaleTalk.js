1f337fa9d573b31a6f86adaa445495d0b890e09e
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-09-05 09:51:26 +0800

    Create whaleTalk.js

---
+++ JavaScript/small_projects/whaleTalk.js
@@ -0,0 +1,19 @@
+let input = 'Hi there, you!';
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+let resultArray = [];
+
+for (i=0; i < input.length; i++) {
+  if (input[i] === 'e') {
+    resultArray.push(input[i]);
+  }
+  if (input[i] === 'u') {
+    resultArray.push(input[i]);
+  }
+  for (z=0; z < vowels.length; z++) {
+    if (input[i] === vowels[z]) {
+      // console.log(input[i]);
+      resultArray.push(input[i]);
+    }
+  }
+}
+console.log(resultArray.join('').toUpperCase());
