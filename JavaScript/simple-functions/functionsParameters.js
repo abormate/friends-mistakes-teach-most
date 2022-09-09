f5d0e6f0145538da4b98395671ae464a715371e5
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-09-09 20:11:23 +0800

    Create functionsParameters.js

---
+++ JavaScript/simple-functions/functionsParameters.js
@@ -0,0 +1,10 @@
+const higherOrderFunc = param => {
+  param();
+  return `I just invoked ${param.name} as a callback function!`
+}
+ 
+const anotherFunc = () => {
+  return 'I\'m being invoked by the higher-order function!';
+}
+ 
+higherOrderFunc(anotherFunc);
