247ce87d521f4cf4938e273dde488f8eb1303bad
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-09-12 12:29:09 +0800

    Update arrayIterators.js

--- JavaScript/arrays/arrayIterators.js
+++ JavaScript/arrays/arrayIterators.js
@@ -1,3 +1,7 @@
+/*
+Imagine you had a grocery list and you wanted to know what each item on the list was. You’d have to scan through each row and check for the item. This common task is similar to what we have to do when we want to iterate over, or loop through, an array. One tool at our disposal is the for loop. However, we also have access to built-in array methods which make looping easier.
+*/
+
 const artists = ['Picasso', 'Kahlo', 'Matisse', 'Utamaro'];
 
 artists.forEach(artist => {
