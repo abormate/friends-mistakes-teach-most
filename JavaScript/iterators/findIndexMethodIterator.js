f98c665d04359abf97568c5f91d472b4656ebacd
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-09-14 07:58:20 +0800

    Update findIndexMethodIterator.js

--- JavaScript/iterators/findIndexMethodIterator.js
+++ JavaScript/iterators/findIndexMethodIterator.js
@@ -1,3 +1,7 @@
+/*
+We sometimes want to find the location of an element in an array. That’s where the .findIndex() method comes in! Calling .findIndex() on an array will return the index of the first element that evaluates to true in the callback function.
+*/
+
 const jumbledNums = [123, 25, 78, 5, 9]; 
  
 const lessThanTen = jumbledNums.findIndex(num => {
