3627ca6b084eb793b7ae6166b1783a25c181f487
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-09-13 09:48:28 +0800

    Update simpleForEachIterator.js

--- JavaScript/arrays/simpleForEachIterator.js
+++ JavaScript/arrays/simpleForEachIterator.js
@@ -1,3 +1,5 @@
+// function defined beforehand and then first function is passed as a callback function for the forEach() higher-order function later on
+
 function printGrocery(element){
   console.log(element);
 }
