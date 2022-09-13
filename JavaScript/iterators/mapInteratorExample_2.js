7be9efa24e6b064c3f605e2ad3b0ff90c44d5a94
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-09-13 10:29:58 +0800

    Update mapInteratorExample_2.js

--- JavaScript/iterators/mapInteratorExample_2.js
+++ JavaScript/iterators/mapInteratorExample_2.js
@@ -5,9 +5,13 @@ const secretMessage = animals.map(letter => letter[0]);
 
 console.log(secretMessage.join(''));
 
+// Outputs Hello World
+
 const bigNumbers = [100, 200, 300, 400, 500];
 
 // Create the smallNumbers array below
 const smallNumbers = bigNumbers.map( num => num/100);
 
 console.log(smallNumbers);
+
+// Outputs small integers 1 to 5
