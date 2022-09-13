b2da48a736ea6701ce344c4ffdf1333427b73ae5
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-09-13 10:05:04 +0800

    Update mapIteratorExample.js

--- JavaScript/iterators/mapIteratorExample.js
+++ JavaScript/iterators/mapIteratorExample.js
@@ -1,3 +1,7 @@
+/*
+.map() works in a similar manner to .forEach()— the major difference is that .map() returns a new array.
+*/
+
 const numbers = [1, 2, 3, 4, 5]; 
  
 const bigNumbers = numbers.map(number => {
