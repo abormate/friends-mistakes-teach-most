8f3c3f1daa4e871b3ec6fab484008e45bf9acce3
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-09-13 11:37:04 +0800

    Update filterMethodIterator.js

--- JavaScript/iterators/filterMethodIterator.js
+++ JavaScript/iterators/filterMethodIterator.js
@@ -5,6 +5,8 @@ const smallNumbers = randomNumbers.filter(num => {
   return num < 250;
 });
 
+// returns an array with numbers < 250
+
 const favoriteWords = ['nostalgia', 'hyperbole', 'fervent', 'esoteric', 'serene'];
 
 
@@ -12,3 +14,5 @@ const favoriteWords = ['nostalgia', 'hyperbole', 'fervent', 'esoteric', 'serene'
 const longFavoriteWords = favoriteWords.filter(word => {
   return word.length > 7;
 });
+
+// returns an array of words with more than 7 characters
