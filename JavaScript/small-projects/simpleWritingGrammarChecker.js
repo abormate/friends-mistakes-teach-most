1150770ed15269110453eb0a506524d1684f01fc
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-09-21 19:05:18 +0800

    Update simpleWritingGrammarChecker.js

--- JavaScript/small-projects/simpleWritingGrammarChecker.js
+++ JavaScript/small-projects/simpleWritingGrammarChecker.js
@@ -20,6 +20,15 @@ The .findIndex() callback function should check each word to see if it equals th
 Then, log badWordIndex to the console.
 */
 
+/*
+
+Now that we have the index of the bad word, we can easily replace it with something more appropriate.
+
+Access the element inside the storyWords array that has the index of badWordIndex using bracket notation. Set the accessed element equal to the more appropriate string, 'really'.
+
+Save the code and check that the bad word has been replaced.
+*/
+
 let story = 'Last weekend, I took literally the most beautifull bike ride of my life. The route is called "The 9W to Nyack" and it stretches all the way from Riverside Park in Manhattan to South Nyack, New Jersey. It\'s really an adventure from beginning to end! It is a 48 mile loop and it literally took me an entire day. I stopped at Riverbank State Park to take some artsy photos. It was a short stop, though, because I had a freaking long way to go. After a quick photo op at the very popular Little Red Lighthouse I began my trek across the George Washington Bridge into New Jersey. The GW is a breathtaking 4,760 feet long! I was already very tired by the time I got to the other side. An hour later, I reached Greenbrook Nature Sanctuary, an extremely beautifull park along the coast of the Hudson. Something that was very surprising to me was that near the end of the route you literally cross back into New York! At this point, you are very close to the end.';
 
 let storyWords = story.split(' ');
