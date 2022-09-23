a389d337d80f22e79cd3cf9acb0b0959b29e14c9
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-09-23 12:41:57 +0800

    Update objectDefinition.js

--- JavaScript/objects/objectDefinition.js
+++ JavaScript/objects/objectDefinition.js
@@ -6,4 +6,7 @@ let spaceship = {
   flightPath: ['Venus', 'Mars', 'Saturn']
 };
 
+const crewCount = spaceship.numCrew;
+const planetArray = spaceship.flightPath;
+
 // Stores an object with various properties to a variable 
