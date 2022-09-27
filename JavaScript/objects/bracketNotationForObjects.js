42774b948fa4f6fb8fca335404e7c7960117de1b
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-09-27 10:05:51 +0800

    Create bracketNotationForObjects.js

---
+++ JavaScript/objects/bracketNotationForObjects.js
@@ -0,0 +1,11 @@
+let spaceship = {
+  'Fuel Type' : 'Turbo Fuel',
+  'Active Mission' : true,
+  homePlanet : 'Earth', 
+  numCrew: 5
+ };
+
+let propName =  'Active Mission';
+
+// Write your code below
+const isActive = spaceship['Active Mission'];
