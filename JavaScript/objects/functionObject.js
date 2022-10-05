9797e2e79a1b2fbe53463ce1bc337eb3bbdb1b55
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-10-05 09:16:46 +0800

    Create functionObject.js

---
+++ JavaScript/objects/functionObject.js
@@ -0,0 +1,8 @@
+let retreatMessage = 'We no longer wish to conquer your planet. It is full of dogs, which we do not care for.';
+
+// Write your code below
+const alienShip = {
+  retreat : function () {
+    console.log(retreatMessage)
+  }
+};
