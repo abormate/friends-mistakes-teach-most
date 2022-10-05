f77e7ed2e8d5cff2e0293bbf40f8db6786f58d08
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-10-06 05:48:50 +0800

    Update functionsAndObjects.js

--- JavaScript/objects/functionsAndObjects.js
+++ JavaScript/objects/functionsAndObjects.js
@@ -10,3 +10,6 @@ const goat = {
  
 goat.diet(); 
 // Output: herbivore
+/*
+The this keyword references the calling object which provides access to the calling object’s properties. In the example above, the calling object is goat and by using this we’re accessing the goat object itself, and then the dietType property of goat by using property dot notation.
+*/
