bd07f031f036790dcc6fee6f71c5f47f4a353cae
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-10-06 05:48:08 +0800

    Create functionsAndObjects.js

---
+++ JavaScript/objects/functionsAndObjects.js
@@ -0,0 +1,12 @@
+const goat = {
+  dietType: 'herbivore',
+  makeSound() {
+    console.log('baaa');
+  },
+  diet() {
+    console.log(this.dietType);
+  }
+};
+ 
+goat.diet(); 
+// Output: herbivore
