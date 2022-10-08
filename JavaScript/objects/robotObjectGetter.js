cfa5b56c6b9b42700763a0763195b77f393cc872
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-10-08 17:26:28 +0800

    Update robotObjectGetter.js

--- JavaScript/objects/robotObjectGetter.js
+++ JavaScript/objects/robotObjectGetter.js
@@ -2,7 +2,9 @@ const robot = {
   _model: '1E78V2',
   _energyLevel: 100,
   get energyLevel() {
-
+    if (typeof this._energyLevel === 'number') {
+      return 'My current energy level is ' + this._energyLevel;
+    }
   }
 };
 
