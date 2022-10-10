2c383ce1c2506e13a2eb86588d5e378b9c6c73dc
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-10-10 10:04:39 +0800

    Update objectMethodSetter.js

--- JavaScript/objects/objectMethodSetter.js
+++ JavaScript/objects/objectMethodSetter.js
@@ -1,3 +1,7 @@
+/*
+Along with getter methods, we can also create setter methods which reassign values of existing properties within an object. Let’s see an example of a setter method:
+*/
+
 const person = {
   _age: 37,
   set age(newAge){
