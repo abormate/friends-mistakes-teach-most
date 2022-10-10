65dff1e33cda5c038a07983ce8a8326f558a4c08
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-10-11 05:48:37 +0800

    Update objectDestructuredVariables.js

--- JavaScript/objects/objectDestructuredVariables.js
+++ JavaScript/objects/objectDestructuredVariables.js
@@ -1,3 +1,7 @@
+/*
+However, we can also take advantage of a destructuring technique called destructured assignment to save ourselves some keystrokes. In destructured assignment we create a variable with the name of an object’s key that is wrapped in curly braces { } and assign to it the object. Take a look at the example below:
+*/
+
 const robot = {
   model: '1E78V2',
   energyLevel: 100,
