0448934b441f933ae66150517611285402acdd7e
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-10-10 10:04:07 +0800

    Create objectMethodSetter.js

---
+++ JavaScript/objects/objectMethodSetter.js
@@ -0,0 +1,10 @@
+const person = {
+  _age: 37,
+  set age(newAge){
+    if (typeof newAge === 'number'){
+      this._age = newAge;
+    } else {
+      console.log('You must assign a number to age');
+    }
+  }
+};
