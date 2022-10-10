48058039fa942ce6739e10caab3c5962353c529f
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-10-11 05:19:12 +0800

    Update objectShorthandValueDestructuring.js

--- JavaScript/objects/objectShorthandValueDestructuring.js
+++ JavaScript/objects/objectShorthandValueDestructuring.js
@@ -12,3 +12,5 @@ function robotFactory(model, mobile){
 const newRobot = robotFactory('P-501', false)
 console.log(newRobot.model)
 console.log(newRobot.mobile)
+
+// Notice that the key:value pair that's normally the definition of a property of an object -- now has no more keys. Because the values are the same with the parameters or arguments of the preceding function.
