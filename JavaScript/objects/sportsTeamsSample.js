ff758f79027bc782670c3c54a7e0f1f0d304f056
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-10-13 06:00:46 +0800

    Update sportsTeamsSample.js

--- JavaScript/objects/sportsTeamsSample.js
+++ JavaScript/objects/sportsTeamsSample.js
@@ -2,6 +2,9 @@ const team = {
   _players: [{firstName: 'Paul', lastName: 'Pierce', age: 35}, {firstName: 'David', lastName: 'Matthews', age: 55}, {firstName: 'James', lastName: 'Peters', age: 48} ],
   _games: [{opponent: 'South Rockets', teamPoints: 75, opponentPoints: 73}, {opponent: 'Boston Celts', teamPoints: 82, opponentPoints: 79}, {opponent: 'Atlantic Taxi', teamPoints: 55, opponentPoints: 63}],
   get players() {
-
+    return (this._players);
   },
+  get games() {
+    return (this._games);
+  }
 };
