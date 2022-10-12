d1becdfc4d40df15cf44e6e258fe5d875b05b6ad
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-10-12 09:21:46 +0800

    Create restaurantMealMakerObject.js

---
+++ JavaScript/objects/restaurantMealMakerObject.js
@@ -0,0 +1,14 @@
+const menu = {
+  _meal: '',
+  _price: 0,
+  set meal(mealToCheck) {
+    if (typeof mealToCheck === string) {
+      return this._meal = mealToCheck;
+    }
+  },
+  set price(priceToCheck) {
+    if (typeof priceToCheck === number) {
+      return this._price = priceToCheck;
+    }
+  }
+};
