a7c8c6611062528d1a43d1af4d1ac54bb99865a5
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-10-12 09:33:53 +0800

    Update restaurantMealMakerObject.js

--- JavaScript/objects/restaurantMealMakerObject.js
+++ JavaScript/objects/restaurantMealMakerObject.js
@@ -2,13 +2,16 @@ const menu = {
   _meal: '',
   _price: 0,
   set meal(mealToCheck) {
-    if (typeof mealToCheck === string) {
+    if (typeof mealToCheck === 'string') {
       return this._meal = mealToCheck;
     }
   },
   set price(priceToCheck) {
-    if (typeof priceToCheck === number) {
+    if (typeof priceToCheck === 'number') {
       return this._price = priceToCheck;
     }
+  },
+  get todaysSpecial() {
+    
   }
 };
