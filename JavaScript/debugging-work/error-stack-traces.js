1ee90c608c2154e14cbf601ca7833cc61b0bc13e
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-11-02 19:23:00 +0800

    Update error-stack-traces.js

--- JavaScript/debugging-work/error-stack-traces.js
+++ JavaScript/debugging-work/error-stack-traces.js
@@ -1,4 +1,5 @@
-Error Stack Traces
+<h1>Error Stack Traces</h1>
+<p>
 We’ll start this lesson by taking a closer look at the most straightforward way to know your code isn’t working as expected: errors!
 
 You might recognize errors as the scary red text that appears on your screen when you try to run broken code. A piece of software, called a compiler, is trying to translate your code so that your computer can understand and run it. However, the compiler is coming across a piece of code that it can’t interpret. As a result, it throws an error back to you to let you know that it has to stop and why.
@@ -8,3 +9,4 @@ This information is logged as an error stack trace — a printed message contain
 Take a look at the diagram to the right to see where you can find all of this information.
 
 As a new programmer, it’s easy to see an error and be overcome with a sense of dread and self-doubt. However, you’ll soon realize, errors are the best kind of bug you can hope for, since they tell you exactly what went wrong and where.
+</p>
