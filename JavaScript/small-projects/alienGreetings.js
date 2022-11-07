7ba23082f8469525a713d8076cdf059e7ef129a8
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-11-07 10:11:45 +0800

    Create alienGreetings.js

---
+++ JavaScript/small-projects/alienGreetings.js
@@ -0,0 +1,7 @@
+let greetAliens = (array) => {
+  result = [];
+  for (let i = 0; i < array.length; i++) {
+    console.log('Oh powerful ' + array[i] + ', we humans offer our unconditional surrender!');
+  }
+  return result;
+}
