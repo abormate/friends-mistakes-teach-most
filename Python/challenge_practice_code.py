f693425072e2ebdafe8371e7255e9848b44dcc4b
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-02-11 13:47:31 +0800

    Update challenge_practice_code.py

--- Python/challenge_practice_code.py
+++ Python/challenge_practice_code.py
@@ -9,3 +9,11 @@ For example, if my_list was [23, 42, 108], the function should return [23, 42, 1
 def append_size(my_list):
   my_list.append(len(my_list))
   return my_list
+
+"""
+Write a function named append_sum that has one parameter — a list named named my_list.
+
+The function should add the last two elements of my_list together and append the result to my_list. It should do this process three times and then return my_list.
+
+For example, if my_list started as [1, 1, 2], the final result should be [1, 1, 2, 3, 5, 8].
+"""
