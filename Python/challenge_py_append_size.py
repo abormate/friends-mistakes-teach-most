4402d91a6c57ed76b43921a276be007bcdf8ef49
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-02-11 13:43:40 +0800

    Update challenge_py_append_size.py

--- Python/challenge_py_append_size.py
+++ Python/challenge_py_append_size.py
@@ -5,3 +5,7 @@ The function should append the size of my_list (inclusive) to the end of my_list
 
 For example, if my_list was [23, 42, 108], the function should return [23, 42, 108, 3] because the size of my_list was originally 3.
 """
+
+def append_size(my_list):
+  my_list.append(len(my_list))
+  return my_list
