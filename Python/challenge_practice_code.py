0b067a6f06f1a53870bebff199965a9170bc7d2c
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-02-11 14:06:44 +0800

    Update challenge_practice_code.py

--- Python/challenge_practice_code.py
+++ Python/challenge_practice_code.py
@@ -67,4 +67,9 @@ Count the number of occurrences of item (the second parameter) in my_list (the f
 If the number of occurrences is greater than n (the third parameter), return True. Otherwise, return False
 """
 
+'''
+Create a function named more_than_n that has three parameters named my_list, item, and n.
+
+The function should return True if item appears in the list more than n times. The function should return False otherwise.
+'''
 
